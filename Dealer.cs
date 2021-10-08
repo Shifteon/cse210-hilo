a89b9c77b9371a5a3e52a03e4f2737de3c286c3f
Mosiah Hazen <haz17005@byui.edu>
2021-10-08 14:23:49 -0600

    The draw and check card functions

--- Dealer.cs
+++ Dealer.cs
@@ -4,6 +4,34 @@ namespace cse210_hilo
 {
     class Dealer
     {
-        
+        int previousCard = 0;
+        public int DealNextCard()
+        {
+            Random cardDraw = new Random();
+            int card;
+            bool cardNotSame = false;
+            while (cardNotSame);
+            {
+                card = cardDraw.Next(1, 14);
+                
+                cardNotSame = IsDifferentCard(card, previousCard);
+
+                
+            }
+            previousCard = card;
+            return card;
+        }
+
+        public bool IsDifferentCard(int card, int previoiusCard)
+        {
+            if (card == previoiusCard)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
